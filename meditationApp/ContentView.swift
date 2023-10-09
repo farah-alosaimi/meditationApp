641d0ea55d81d1fbf5d54e3eae7035076021d3c2
farah-alosaimi <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>
2023-10-09 10:29:26 +0300

    Initial Commit

---
+++ meditationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meditationApp
+//
+//  Created by Farah Hasan Alosaimi on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meditationApp/meditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditationAppApp.swift
+//  meditationApp
+//
+//  Created by Farah Hasan Alosaimi on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct meditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
