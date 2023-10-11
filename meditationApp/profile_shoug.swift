34f8d663a3d4bc876f642b8a820568fe2dfb43bd
shoug <sho0oshkh@gmail.com>
2023-10-11 11:06:42 +0300

    commmit_shougf

---
+++ meditationApp/profile_shoug.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  meditationApp
+//
+//  Created by shoug on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
